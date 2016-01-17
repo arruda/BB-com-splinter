1597ddab0ca82b8d08edb5a5734de391edefae71
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2016-01-17 05:10:26 -0200

    simple python script that opens a ff browser using a custom profile

---
+++ bb.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from splinter import Browser
+
+
+if __name__ == "__main__":
+    b = Browser(
+        'firefox',
+        profile='/root/.mozilla/firefox/myprof.default',
+        profile_preferences={
+            'security.enable_java': True,
+            'plugin.state.java': 2,
+        }
+    )
+    import pdb; pdb.set_trace()
+    b.windows[0].close()
