5e21aca840570076a1ab7a51de33a17fcab95b05
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2016-01-17 22:04:55 -0200

    um pouco de navegação

--- bb.py
+++ bb.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
+import os
 from splinter import Browser
+
+AG = os.environ.get('AG')
+CC = os.environ.get('CC')
+SE = os.environ.get('SE')
 
 
 if __name__ == "__main__":
@@ -10,5 +15,17 @@ if __name__ == "__main__":
             'plugin.state.java': 2,
         }
     )
-    import pdb; pdb.set_trace()
+    import ipdb; ipdb.set_trace()
+    b.driver.set_page_load_timeout(8)
+    # driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+    b.visit('https://www2.bancobrasil.com.br/aapf/login.jsp')
+
+    b.fill('dependenciaOrigem', AG)
+    b.fill('numeroContratoOrigem', CC)
+    b.fill('senhaConta', SE)
+    b.find_by_id('botaoEntrar').click()
+    import ipdb; ipdb.set_trace()
     b.windows[0].close()
+
+    # form = b.find_by_id('aapfModal').first
+    # bt_fechar_modal = form.find_by_css('.botaoToolBarFechar').first
